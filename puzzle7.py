67f4028b3f785546ec0049a50dca415401ec51d4
Andreas Pakulat <andreas@froglogic.com>
2017-12-07 23:49:36 +0100

    Use a dict for the tree
    I expect this to make part2 a little easier to write

--- puzzle7.py
+++ puzzle7.py
@@ -2,7 +2,7 @@ import sys
 import re
 
 def parse(input):
-    nodes = []
+    nodes = {}
     for line in input.split('\n'):
         weightidx = line.find('(')+1
         name = line[:line.find(' ')]
@@ -12,19 +12,19 @@ def parse(input):
             childs = [x.strip() for x in line[childidx+2:].split(',')]
         else:
             childs = []
-        nodes.append(((name, weight), childs))
+        nodes[name] = (weight, childs)
     return nodes
 
 def reduceTree(tree):
-    leaves = [x[0][0] for x in filter(lambda x: len(x[1]) == 0, tree)]
-    nonleaves = filter(lambda x: len(x[1]) > 0, tree)
-    newtree = map(lambda x: (x[0], filter(lambda y: y not in leaves, x[1])), nonleaves)
+    leaves = dict(filter(lambda x: len(x[1][1]) == 0, tree.items()))
+    nonleaves = dict(filter(lambda x: len(x[1][1]) > 0, tree.items()))
+    newtree = dict(map(lambda x: (x[0], (x[1][0], filter(lambda y: y not in leaves, x[1][1]))), nonleaves.items()))
     if len(newtree) == 1:
         return newtree
     return reduceTree(newtree)
 
 def part1(input):
-    return "%s" % (reduceTree(parse(input))[0][0][0],)
+    return "%s" % (reduceTree(parse(input)))
 
 if __name__ == "__main__":
     sys.stdout.write(part1(sys.stdin.read().strip()) + '\n')
