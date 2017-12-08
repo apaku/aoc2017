d18d9d24ff4da91ff3baefc1882581c67a18c555
Andreas Pakulat <andreas@froglogic.com>
2017-12-08 13:59:46 +0100

    Share code between part 1 and 2 of day 8
    part1 is trivially implemented with the result of part2, so lets avoid
    the duplication.

--- puzzle8.py
+++ puzzle8.py
@@ -3,16 +3,7 @@ import sys
 
 ops = {'inc': '+', 'dec': '-'}
 
-def part1(input):
-    registers = defaultdict(lambda: 0)
-    for line in input.split('\n'):
-        (reg, op, value, _, left, condition, right) = tuple(line.split(' '))
-
-        if eval("registers['%s'] %s %s" % (left, condition, right)):
-            registers[reg] = eval("registers['{}'] {} {}".format(reg, ops[op], value))
-    return "%s" % max(registers.values())
-
-def part2(input):
+def maximums(input):
     registers = defaultdict(lambda: 0)
     maximums = []
     for line in input.split('\n'):
@@ -21,6 +12,13 @@ def part2(input):
         if eval("registers['%s'] %s %s" % (left, condition, right)):
             registers[reg] = eval("registers['{}'] {} {}".format(reg, ops[op], value))
         maximums.append(max(registers.values()))
-    return "%s" % max(maximums)
+    return maximums
+
+def part1(input):
+    return "%s" % maximums(input)[-1]
+
+def part2(input):
+    return "%s" % max(maximums(input))
+
 if __name__ == "__main__":
     sys.stdout.write(part2(sys.stdin.read().strip()) + "\n")
