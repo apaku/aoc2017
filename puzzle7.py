3a6a01a2256d83cf40c3b3cf5867085fb4c3ddde
Andreas Pakulat <andreas@froglogic.com>
2017-12-08 01:15:31 +0100

    part 2 of day 7
    This was quite a bit involved and I think the code can be made a lot
    easier/nicer. In particular I don't like it that the weight-calculation
    is done twice when the code decides it has to recurse. And it does not
    work if the root node is the one needing balancing either (i.e. the
    list of unbalanced childs is empty). Oh well, for another day.

--- puzzle7.py
+++ puzzle7.py
@@ -1,5 +1,6 @@
 import sys
 import re
+from collections import defaultdict
 
 def parse(input):
     nodes = {}
@@ -26,5 +27,42 @@ def reduceTree(tree):
 def part1(input):
     return "%s" % (reduceTree(parse(input)))
 
+def nodeWeight(node, tree):
+    return tree[node][0] + sum(map(lambda x: nodeWeight(x, tree), tree[node][1]))
+
+def determineBalance(startNode, tree):
+    weights = defaultdict(lambda: [])
+    for child in tree[startNode][1]:
+        weight = nodeWeight(child, tree)
+        weights[weight].append(child)
+    return (filter(lambda x: len(x[1]) > 1, weights.items()),
+            filter(lambda x: len(x[1]) == 1, weights.items())
+    )
+
+def unbalancedChild(node, tree):
+    (_, unbalanced) = determineBalance(node, tree)
+    if len(unbalanced) == 0:
+        return None
+    return unbalanced[0][1][0]
+
+def findImbalance(node, tree):
+    (balanced, unbalanced) = determineBalance(node, tree)
+
+    (unbalWeight, unbalNames) = unbalanced[0]
+    (balWeight, balNames) = balanced[0]
+
+    childNode = unbalancedChild(unbalNames[0], tree)
+    if childNode:
+        return findImbalance(childNode, tree)
+
+    diff = balWeight - unbalWeight
+    return (unbalNames[0], diff, tree[unbalNames[0]][0] + diff)
+
+def part2(input):
+    tree = parse(input)
+    rootNode = reduceTree(tree)
+    print findImbalance(rootNode.keys()[0], tree)
+    return ""
+
 if __name__ == "__main__":
-    sys.stdout.write(part1(sys.stdin.read().strip()) + '\n')
+    sys.stdout.write(part2(sys.stdin.read().strip()) + '\n')
