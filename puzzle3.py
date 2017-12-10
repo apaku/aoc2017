6ccb7e61f4393cf5592588383e89cc5bdd16bfeb
Andreas Pakulat <andreas@froglogic.com>
2017-12-10 12:40:10 +0100

    Put in part2 of day3
    Finally found some time to think about how to do the second part, rather
    trivial with the spiral I 'stole'.

--- puzzle3.py
+++ puzzle3.py
@@ -28,12 +28,23 @@ def indices():
         yield pos
         pos = move(where, pos)
 
-def main(input):
+def part1(input):
     distance = ""
     for i, (x,y) in enumerate(indices()):
         if i == int(input)-1:
             distance = "%s" %(abs(x) + abs(y))
             break
     return distance
+def part2(input):
+    values = {(0,0): 1}
+    for i, (x,y) in enumerate(indices()):
+        curvalue = 0
+        if i != 0:
+            for entry in [(x-1,y), (x-1,y-1), (x, y-1), (x+1,y), (x+1, y+1), (x, y+1), (x+1, y-1), (x-1, y+1)]:
+                curvalue += values[entry] if entry in values.keys() else 0
+            values[(x,y)] = curvalue
+        if curvalue > int(input):
+            return curvalue
+    assert False
 if __name__ == "__main__":
-    sys.stdout.write(main(sys.stdin.read().strip())+"\n")
+    print(part2(sys.stdin.read().strip()))
