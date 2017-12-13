afd516810b2100d2c1eefd0fdecfd2d7b737d956
Andreas Pakulat <andreas@froglogic.com>
2017-12-13 13:56:53 +0100

    Abort as soon as the first scanner hitting me has been found
    There's no point to find all of them for the second part of the puzzle, so
    just stop early. Down from 40 seconds to 8.

--- puzzle13.py
+++ puzzle13.py
@@ -3,18 +3,20 @@ import sys
 def parse(input):
     return dict([(int(line.split(": ")[0]), int(line.split(": ")[1])) for line in input.split("\n")])
 
-def scannersAtZeroWhenPassingWithDelay(delay, scannerConfig):
+def scannersAtZeroWhenPassingWithDelay(delay, scannerConfig, firstOnly):
     scannersAtZero = []
     for (scannerPos, scannerMax) in scannerConfig.items():
         positionAtPass2 = (delay + scannerPos) % ((scannerMax - 1) * 2)
         if positionAtPass2 == 0:
+            if firstOnly:
+                return [(scannerPos, scannerMax)]
             scannersAtZero.append((scannerPos, scannerMax))
     return scannersAtZero
 
 def findDelay(config):
     delay = 0
     while True:
-        scannersAtZero = scannersAtZeroWhenPassingWithDelay(delay, config)
+        scannersAtZero = scannersAtZeroWhenPassingWithDelay(delay, config, True)
         if len(scannersAtZero) == 0:
             return delay
         delay += 1
@@ -22,7 +24,7 @@ def findDelay(config):
 
 def doit(input):
     firewallconfig = parse(input)
-    part1 = sum([x * y for (x,y) in scannersAtZeroWhenPassingWithDelay(0, firewallconfig)])
+    part1 = sum([x * y for (x,y) in scannersAtZeroWhenPassingWithDelay(0, firewallconfig, False)])
     part2 = findDelay(firewallconfig)
     return part1, part2
 
