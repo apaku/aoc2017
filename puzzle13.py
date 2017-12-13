c00ff91c508274bcbac3ef983b261dd1028e1192
Andreas Pakulat <andreas@froglogic.com>
2017-12-13 09:28:44 +0100

    Attempt to solve part2
    Unfortunately only works for the input sample, the real input runs
    endlessly so there's some error in my thinking but I can't spot it
    right now.

--- puzzle13.py
+++ puzzle13.py
@@ -4,20 +4,20 @@ def parse(input):
     return dict([(int(line.split(": ")[0]), int(line.split(": ")[1])) for line in input.split("\n")])
 
 def nextState(oldstate, firewallconfig):
+    newstate = dict(oldstate)
     for key in firewallconfig.keys():
-        oldstate[key] = (oldstate[key][0] + oldstate[key][1], oldstate[key][1])
-        if oldstate[key][0] == firewallconfig[key]:
-            oldstate[key] = (oldstate[key][0] - 2, -1)
-        elif oldstate[key][0] == -1:
-            oldstate[key] = (oldstate[key][0] + 2, 1)
-    return oldstate
+        newstate[key] = (newstate[key][0] + newstate[key][1], newstate[key][1])
+        if newstate[key][0] == firewallconfig[key]:
+            newstate[key] = (newstate[key][0] - 2, -1)
+        elif newstate[key][0] == -1:
+            newstate[key] = (newstate[key][0] + 2, 1)
+    return newstate
 
 def initialState(config):
     return dict([(k, (0, 1)) for k in config.keys()])
 
-def part1(input):
-    firewallconfig = parse(input)
-    state = initialState(firewallconfig)
+def tryTravel(initialState, firewallconfig):
+    state = dict(initialState)
     caughtLayers = []
     packetPos = -1
     while packetPos < max(firewallconfig.keys()):
@@ -26,5 +26,26 @@ def part1(input):
             caughtLayers.append(packetPos)
         state = nextState(state, firewallconfig)
     return caughtLayers, sum([k*firewallconfig[k] for k in caughtLayers])
+
+def findDelay(initState, config):
+    state = dict(initState)
+    delay = 0
+    while True:
+        print "1", state
+        (caught, severity) = tryTravel(state, config)
+        print "2", caught, severity
+        if len(caught) == 0:
+            return delay
+        delay += 1
+        state = nextState(state, config)
+
+def doit(input):
+    firewallconfig = parse(input)
+    state = initialState(firewallconfig)
+    part1 = tryTravel(state, firewallconfig)
+    print "------------------------------"
+    part2 = findDelay(state, firewallconfig)
+    return part1, part2
+
 if __name__ == "__main__":
-    print(part1(sys.stdin.read().strip()))
+    print(doit(sys.stdin.read().strip()))
