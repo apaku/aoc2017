13cc05ced65e64c2bf598eecb9a41c9bd3b46426
Andreas Pakulat <andreas@froglogic.com>
2017-12-13 13:56:12 +0100

    Get rid of the exponential calculation of the intermediate positions of the scanner
    After some vague tips from a colleague I finally realized how to determine
    the final position of a given scanner after a given time with just basic
    maths.
    
    This brings down calculation of the real input from half a day to about
    40 seconds.

--- puzzle13.py
+++ puzzle13.py
@@ -3,66 +3,15 @@ import sys
 def parse(input):
     return dict([(int(line.split(": ")[0]), int(line.split(": ")[1])) for line in input.split("\n")])
 
-def nextPositionOfScanner(scannerIdx, direction, scannerMax):
-    print "    nextPos:", scannerIdx, direction, scannerMax
-    if scannerIdx + direction < 0:
-        return (1, -1 * direction)
-    elif scannerIdx + direction == scannerMax:
-        return (scannerIdx - 1, -1 * direction)
-    return (scannerIdx + direction, direction)
-
-def positionOfScannerAt(picoSeconds, scannerLength):
-    scannerIdx = 0
-    direction = 1
-    for i in range(0, picoSeconds):
-        (scannerIdx, direction) = nextPositionOfScanner(scannerIdx, direction, scannerLength)
-    return scannerIdx
-
 def scannersAtZeroWhenPassingWithDelay(delay, scannerConfig):
     scannersAtZero = []
     for (scannerPos, scannerMax) in scannerConfig.items():
-        print "Scanner at ", scannerPos, "calculating for", delay, "scanner's size", scannerMax
-        positionAtPass = positionOfScannerAt(delay + scannerPos, scannerMax)
-        print "Position of scanner:", positionAtPass
-        if positionAtPass == 0:
+        positionAtPass2 = (delay + scannerPos) % ((scannerMax - 1) * 2)
+        if positionAtPass2 == 0:
             scannersAtZero.append((scannerPos, scannerMax))
     return scannersAtZero
 
-def nextState(oldstate, firewallconfig):
-    newstate = dict(oldstate)
-    for key in firewallconfig.keys():
-        newstate[key] = (newstate[key][0] + newstate[key][1], newstate[key][1])
-        if newstate[key][0] == firewallconfig[key]:
-            newstate[key] = (newstate[key][0] - 2, -1)
-        elif newstate[key][0] == -1:
-            newstate[key] = (newstate[key][0] + 2, 1)
-    return newstate
-
-def initialState(config):
-    return dict([(k, (0, 1)) for k in config.keys()])
-
-def tryTravel(initialState, firewallconfig):
-    state = dict(initialState)
-    caughtLayers = []
-    packetPos = -1
-    while packetPos < max(firewallconfig.keys()):
-        packetPos += 1
-        if packetPos in state.keys() and state[packetPos][0] == 0:
-            caughtLayers.append(packetPos)
-        state = nextState(state, firewallconfig)
-    return caughtLayers, sum([k*firewallconfig[k] for k in caughtLayers])
-
-def findDelay(initState, config):
-    state = dict(initState)
-    delay = 0
-    while True:
-        (caught, severity) = tryTravel(state, config)
-        if len(caught) == 0:
-            return delay
-        delay += 1
-        state = nextState(state, config)
-
-def findDelay2(config):
+def findDelay(config):
     delay = 0
     while True:
         scannersAtZero = scannersAtZeroWhenPassingWithDelay(delay, config)
@@ -73,9 +22,8 @@ def findDelay2(config):
 
 def doit(input):
     firewallconfig = parse(input)
-    state = initialState(firewallconfig)
     part1 = sum([x * y for (x,y) in scannersAtZeroWhenPassingWithDelay(0, firewallconfig)])
-    part2 = findDelay2(firewallconfig)
+    part2 = findDelay(firewallconfig)
     return part1, part2
 
 if __name__ == "__main__":
