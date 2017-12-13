b105661dbbbc5686c601fa254988b29b97b1cddd
Andreas Pakulat <andreas@froglogic.com>
2017-12-13 13:16:05 +0100

    Alternative approach to make it a little easier to follow
    This one allows to calculate the position of a scanner for a given delay,
    allowing to figure out the list of scanners being at 0 when they're being
    passed.
    
    The rest is still brute force trying out wether the list of scanners gets
    empty. Unfortunately still the same issue leading to a seemingly endless
    calculation.

--- puzzle13.py
+++ puzzle13.py
@@ -3,6 +3,31 @@ import sys
 def parse(input):
     return dict([(int(line.split(": ")[0]), int(line.split(": ")[1])) for line in input.split("\n")])
 
+def nextPositionOfScanner(scannerIdx, direction, scannerMax):
+    print "    nextPos:", scannerIdx, direction, scannerMax
+    if scannerIdx + direction < 0:
+        return (1, -1 * direction)
+    elif scannerIdx + direction == scannerMax:
+        return (scannerIdx - 1, -1 * direction)
+    return (scannerIdx + direction, direction)
+
+def positionOfScannerAt(picoSeconds, scannerLength):
+    scannerIdx = 0
+    direction = 1
+    for i in range(0, picoSeconds):
+        (scannerIdx, direction) = nextPositionOfScanner(scannerIdx, direction, scannerLength)
+    return scannerIdx
+
+def scannersAtZeroWhenPassingWithDelay(delay, scannerConfig):
+    scannersAtZero = []
+    for (scannerPos, scannerMax) in scannerConfig.items():
+        print "Scanner at ", scannerPos, "calculating for", delay, "scanner's size", scannerMax
+        positionAtPass = positionOfScannerAt(delay + scannerPos, scannerMax)
+        print "Position of scanner:", positionAtPass
+        if positionAtPass == 0:
+            scannersAtZero.append((scannerPos, scannerMax))
+    return scannersAtZero
+
 def nextState(oldstate, firewallconfig):
     newstate = dict(oldstate)
     for key in firewallconfig.keys():
@@ -31,20 +56,26 @@ def findDelay(initState, config):
     state = dict(initState)
     delay = 0
     while True:
-        print "1", state
         (caught, severity) = tryTravel(state, config)
-        print "2", caught, severity
         if len(caught) == 0:
             return delay
         delay += 1
         state = nextState(state, config)
 
+def findDelay2(config):
+    delay = 0
+    while True:
+        scannersAtZero = scannersAtZeroWhenPassingWithDelay(delay, config)
+        if len(scannersAtZero) == 0:
+            return delay
+        delay += 1
+
+
 def doit(input):
     firewallconfig = parse(input)
     state = initialState(firewallconfig)
-    part1 = tryTravel(state, firewallconfig)
-    print "------------------------------"
-    part2 = findDelay(state, firewallconfig)
+    part1 = sum([x * y for (x,y) in scannersAtZeroWhenPassingWithDelay(0, firewallconfig)])
+    part2 = findDelay2(firewallconfig)
     return part1, part2
 
 if __name__ == "__main__":
