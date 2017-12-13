49a8b369a36cce4d48d919696baaf30545dea3e6
Andreas Pakulat <andreas@froglogic.com>
2017-12-13 23:51:46 +0100

    Rework the day 13 solution a little
    This is easier on the eyes - imo - and still about as fast as my fastest
    solution.
    
    Instead of the loop to find the scanners hitting I'm using an iterator
    which makes the 'exit on first' vs. 'collect them all' for parts 1 and 2
    a lot nicer to write.
    
    I'm not quite satisfied with findDelay, I think that could look nicer with
    an iterator generating some endless list too, but I cannot think how to
    write that nicely at the moment.

--- puzzle13.py
+++ puzzle13.py
@@ -1,41 +1,29 @@
-import cProfile as profile
-import pstats
-import StringIO
 import sys
 
 def parse(input):
     return [(int(line.split(": ")[0]), int(line.split(": ")[1])) for line in input.split("\n")]
 
-def scannersAtZeroWhenPassingWithDelay(delay, scannerConfig, firstOnly):
-    scannersAtZero = []
-    for (scannerPos, scannerMax) in scannerConfig:
-        positionAtPass2 = (delay + scannerPos) % ((scannerMax - 1) * 2)
-        if positionAtPass2 == 0:
-            if firstOnly:
-                return (False, [(scannerPos, scannerMax)])
-            scannersAtZero.append((scannerPos, scannerMax))
-    return (True, scannersAtZero)
+def scannerPositionAfter(scanner, initialDelay):
+    return (initialDelay + scanner[0]) % ((scanner[1] - 1) * 2)
+
+def scannersHittingMe(scannerConfig, delay):
+    for scanner in scannerConfig:
+        if scannerPositionAfter(scanner, delay) == 0:
+            yield scanner
 
 def findDelay(config):
     delay = 0
     while True:
-        scannersAtZero = scannersAtZeroWhenPassingWithDelay(delay, config, True)
-        if scannersAtZero[0]:
+        try:
+            next(scannersHittingMe(config, delay))
+        except StopIteration:
             return delay
         delay += 1
 
-
 def doit(input):
     firewallconfig = parse(input)
-    part1 = sum([x * y for (x,y) in scannersAtZeroWhenPassingWithDelay(0, firewallconfig, False)[1]])
-    profiler = profile.Profile()
-    profiler.enable()
+    part1 = sum(map(lambda (x,y): x * y, scannersHittingMe(firewallconfig, 0)))
     part2 = findDelay(firewallconfig)
-    profiler.disable()
-    stats = StringIO.StringIO()
-    ps = pstats.Stats(profiler, stream=stats).sort_stats('pcalls')
-    ps.print_stats()
-    print stats.getvalue()
     return part1, part2
 
 if __name__ == "__main__":
