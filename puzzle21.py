dd86376e955dc13d90823508a0104dce51c6e09d
Andreas Pakulat <andreas@froglogic.com>
2017-12-21 22:05:40 +0100

    Finish solving part 1 of day21 - in theory
    Got rotation and flipping working now and it solves the sample but for some
    reason gives me a too high number apparently for my real input.
    
    Unfortunately I don't see where I have an error at the moment.

--- puzzle21.py
+++ puzzle21.py
@@ -1,3 +1,4 @@
+from itertools import chain
 import math
 import sys
 
@@ -36,78 +37,73 @@ def parsePattern(lines):
     grid = []
     for line in lines:
         grid.append([x == '#' for x in line])
-
     return grid
 
 def matchPattern(grid, rules):
     for rule in rules:
         if len(rule[0]) != len(grid[0]):
             continue
-        for rotation in (0, 90,180,270):
-            if rotate(rule[0], rotation) == grid:
-                return rule[1]
-            if flipVertical(rule[0]) == grid:
-                return rule[1]
-            if flipHorizontal(rule[0]) == grid:
-                return rule[1]
-            if flipVertical(flipHorizontal(rule[0])) == grid:
-                return rule[1]
+        if rule[0] == grid:
+            return rule[1]
     return None
+
+def subgrid(grid, offset, size):
+    startline = offset[1] * size
+    startcol = offset[0] * size
+    subgrid = []
+    for i in range(size):
+        subgrid.append(grid[startline+i][startcol:startcol+size])
+    return subgrid
 
 def divide(grid, size):
     assert len(grid) % size == 0
-    numsubgrids = (len(grid) / size) * (len(grid) / size)
-    grids = [[] for i in xrange(0, numsubgrids)]
-    gridrow = 0
-    for i, line in enumerate(grid):
-        sublines = [line[k:k+size] for k in range(0, len(line), size)]
-        for j, subline in enumerate(sublines):
-            grid[gridrow+j].append(subline)
-        if i == size-1:
-            gridrow += 1
+    numsubgridsinline = len(grid) / size
+    grids = []
+    for i in range(numsubgridsinline):
+        for j in range(numsubgridsinline):
+            grids.append(subgrid(grid, (i,j), size))
     return grids
 
 def merge(grids):
-    gridlinelen = math.sqrt(len(grids))
-    supergrid = [[[] for i in range(0,gridlinelen)] for j in range(0, gridlinelen)]
-    supergridrows = [[] for i in range(0,gridlinelen)]
-    for i, grid in enumerate(grids):
-        for j, line in enumerate(grid):
-            supergridrows[j] += line
-        if i + gridlinelen == 0:
-            supergrid += supergridrows
-            supergridrows = [[] for i in range(0,gridlinelen)]
-    return supergrid
+    numsubgridstomerge = int(math.sqrt(len(grids)))
+    grid = []
+    for i in range(0, numsubgridstomerge):
+        merged = [list(chain(*k)) for k in zip(*grids[i:i + numsubgridstomerge])]
+        grid += merged
+    return grid
 
 def parse(line):
-    mapping = line.split(" => ")
+    mapping = line.strip().split(" => ")
     inputgrid = parsePattern(mapping[0].split('/'))
     outputgrid = parsePattern(mapping[1].split('/'))
     return (inputgrid, outputgrid)
 
+def explode(rules):
+    explodedrules = []
+    for rule in rules:
+        for rotation in [0, 90, 180, 270]:
+            explodedrules.append((list(rotate(rule[0], rotation)), rule[1]))
+        for rotation in [0, 90, 180, 270]:
+            explodedrules.append((list(flipVertical(rotate(rule[0], rotation))), rule[1]))
+    return explodedrules
+
 def doit(lines):
     grid = start
     print "original\n", toStr(grid)
-    print "rotated 0\n", toStr(rotate(grid, 0))
-    print "rotated 90\n", toStr(rotate(grid, 90))
-    print "rotated 180\n", toStr(rotate(grid, 180))
-    print "rotated 270\n", toStr(rotate(grid, 270))
-    print "rotated 270\n", toStr(rotate(grid, 360))
-    print "flipped vertically\n", toStr(flipVertical(grid))
-    print "flipped horizontally\n", toStr(flipHorizontal(grid))
-    rules = [parse(line) for line in lines]
+    rules = explode([parse(line) for line in lines])
+    print "rules", len(rules)
     for i in range(5):
-        print "dividing\n", toStr(grid)
         if len(grid) % 2 == 0:
             subgrids = divide(grid, 2)
         else:
             subgrids = divide(grid, 3)
-        for i, grid in enumerate(subgrids):
-            print "matching\n", toStr(subgrid)
-            subgrids[i] = matchPattern(grid, rules)
+        for i, subgrid in enumerate(subgrids):
+            subgrids[i] = matchPattern(subgrid, rules)
             assert subgrids[i] is not None
         grid = merge(subgrids)
-    return len([y for x in grid for y in x if y == 1])
+        print toStr(grid)
+    print "Final grid\n", toStr(grid)
+    return len([y for x in grid for y in x if y])
 
 if __name__ == "__main__":
     print doit(sys.stdin.readlines())
